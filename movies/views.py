5e83daf8162a9129df556e3ea7af118e21b5388a
20morgan <ryanmorgan001@gmail.com>
2019-12-19 13:38:32 -0500

    final project

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.shortcuts import render
 from django.views.generic import CreateView
 from django.views.generic.list import ListView
@@ -11,6 +12,18 @@ class MoviesListView(ListView):
     model = Movie
 
 
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Movie
+        fields = ('title', 'genre', 'rating', 'director')
+        widgets = {'title': forms.TextInput(attrs={'class': 'titleform'}),
+                   'genre': forms.TextInput(attrs={'class': 'genreform'}),
+                   'rating': forms.TextInput(attrs={'class': 'ratingform'}),
+                   'director': forms.TextInput(attrs={'class': 'directorform'})
+                   }
+
+
 class MovieCreateView(CreateView):
     model = Movie
-    fields = ['title', 'genre', 'rating', 'director']
+    # fields = ['title', 'genre', 'rating', 'director']
+    form_class = QuestionForm
